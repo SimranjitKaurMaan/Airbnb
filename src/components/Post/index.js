ee44b27ecc228f98c68b7d20e89895afc2439d14
Simranjit Kaur Maan <simranjitkaur94mec@gmail.com>
2021-05-09 08:42:40 +0530

    Added post props

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,29 +2,34 @@ import React from 'react'
 import { View, Text , Image} from 'react-native'
 import styles from './styles'
 
-const Post = () => {
+const Post = (props) => {
+   console.log(props)
+
+   const post = props.post;
+
     return (
         <View style={styles.container}>
             {/* Image */}
-            <Image style={styles.image} source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/2.jpg'}}></Image>
+            <Image style={styles.image} source={{uri: post.image}}></Image>
 
             {/* Bed and Bedroom */}
-            <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+            <Text style={styles.bedrooms}>
+                {post.bed} bed {post.bedroom} bedroom</Text>
 
             {/* Type and description  */}
             <Text style={styles.description} numberOfLines={2}>
-                Entire Flat.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s 
+                {post.type} . {post.title} {post.description}
             </Text>
 
             {/* Old price and new price */}
             <Text style={styles.prices}>
-                <Text style={styles.oldPrice}>$36</Text>
-                <Text style={styles.price}> $30 </Text>
+                <Text style={styles.oldPrice}>${post.oldPrice}</Text>
+                <Text style={styles.price}> ${post.newPrice} </Text>
                 <Text>/ night</Text>
             </Text>
 
             {/* Total price */}
-            <Text style={styles.totalPrice}>$230 total</Text>
+            <Text style={styles.totalPrice}>${post.totalPrice} total</Text>
         </View>
     )
 }
